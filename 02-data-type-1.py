1d002d6f15ce59c221de667484ff0d3489b19b23
unknown <>
2017-03-12 17:50:22 +0900

    Add 02-date-type-1.py

---
+++ 02-data-type-1.py
@@ -0,0 +1,17 @@
+# int
+print(1+1)
+# float
+print(1.5+2.1)
+# str
+print("hello")
+
+# print("hello" +"1")
+#print(4//2)
+#print(int(4/2))
+#print(float(2))
+print("1")
+print(int("1"))
+print(int("1.5"))
+#1) 1
+# print(int(1.5))
+#2) eroor
