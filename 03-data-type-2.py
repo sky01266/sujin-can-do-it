8b2a96b50fe82f02ec5c1091d5576236d671425e
unknown <>
2017-03-19 10:32:52 +0900

    Add ㄴ.학교 클래스.py

---
+++ 03-data-type-2.py
@@ -0,0 +1,29 @@
+# list, tuple
+# dict-dictionary
+# set
+
+
+list[]
+print("list")
+list_a = [1,2,3]
+print(list_a)
+print(type([1,2,3]))
+# index starts "0"
+print(list_a[0])
+print(list_a[1])
+print(list_a[2])
+# slice
+print(list_a[0:2])
+list_a.append(4)
+print(list_a)
+list_a.remove(2)
+print(list_a)
+list_a.clear()
+print(list_a)
+
+# tuple()
+print("tuple")
+tuple_a=(1,2,3)
+print(tuple_a)
+print(type(tuple_a))
+# tuple_a.append(4)
